1ca4d6e498c9f4f3f8f8d771945f05fcf3928efe
Adrian <ggrin@riseup.net>
2020-05-06 13:19:45 +0200

    have a humidity graph

--- app/index.js
+++ app/index.js
@@ -43,15 +43,55 @@ function FtoC(f) {
   return (f- 32) * 5/9;
 }
 
-async function updateChart() {
-  const data = await Promise.all(subscribed.map( key => getReadings(key) ));
+async function updateChart(since) {
+  const data = await Promise.all(subscribed.map( key => getReadings(key, since) ));
   console.log(data);
-  const container = document.getElementById('chart');
-  var items = data.reduce((acc, device) => [
-    ...acc, ...device.map( ({time, temperature_C:temp, temperature_F, key}) => ({x: new Date(time), y: temp || FtoC(temperature_F), group: key}))
-  ], []);
-  var dataset = new vis.DataSet(items);
-  var graph2d = new vis.Graph2d(container, dataset);
+  const items = {temps: [], humids: [], groups: []};
+  const colors = ['red', 'green', 'blue', 'yellow', 'orange', 'violet']; // should be configurable
+  data.forEach((device) => {
+    if(device.length > 0) {
+      const color = colors.pop();
+      items.groups.push({
+        id: device[0].key,
+        content: device[0].key,
+        style: `stroke:${color};fill:${color}`,
+        options:{
+          drawPoints: { styles: `stroke:${color};fill:${color}`}
+        }
+      });
+    }
+    device.forEach( ({time, temperature_C, temperature_F, humidity, key}, index) => {
+      const item = {x: new Date(time),  group: key};
+      const temp = temperature_C || (temperature_F ? FtoC(temperature_F) : null);
+      if(!temp && !humidity) {
+        console.log(`Bad Reading for ${key}: `, device[index]);
+        return;
+      }
+      if(temp)
+        items.temps.push({...item, y: temp});
+      if(humidity)
+        items.humids.push({...item, y: humidity});
+    });
+  });
+  const temps = new vis.DataSet(items.temps);
+  const groups = new vis.DataSet(items.groups);
+  const opts = {
+    defaultGroup: 'ungrouped',
+    legend: true,
+    drawPoints: {
+      style: 'circle', // square, circle
+      size: 3
+    },
+    dataAxis: {
+      showMinorLabels: true,
+    }
+  };
+  const tempGraph = new vis.Graph2d(document.getElementById('tempChart'), temps, groups, opts);
+  tempGraph.setOptions({dataAxis: {left: { title: {  text: 'Temperature in °C)' }}}});
+  const humids = new vis.DataSet(items.humids);
+  const humidGraph = new vis.Graph2d(document.getElementById('humidChart'), humids, groups, opts);
+  humidGraph.setOptions({dataAxis: {left: { title: {  text: 'Humidity in %)' }}}});
+
 }
 
 async function main() {
