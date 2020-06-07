90d6bfb41df69f32edcf72a477f79707394c5a2f
Niklas Cathor <niklas.cathor@gmx.de>
2020-06-07 16:39:07 +0200

    rtl_client: Preprocess some data, where applicable

--- rtl_client.js
+++ rtl_client.js
@@ -3,6 +3,25 @@ const db = require('./redis');
 
 const server = udp.createSocket('udp4');
 
+function tryParsePayload(payload) {
+  let string = payload.reduce((s, byte) => s + String.fromCharCode(byte), '');
+  try {
+    return JSON.parse(string);
+  } catch(e) {
+    return null;
+  }
+}
+
+function preprocess(data) {
+  data.key = createId(data);
+  if (data.payload && Array.isArray(data.payload)) {
+    let parsed = tryParsePayload(data.payload);
+    if (parsed) {
+      Object.assign(data, parsed);
+    }
+  }
+}
+
 function start (host = '127.0.0.1', port = 1433) {
   server.bind(port, host);
 
@@ -15,7 +34,7 @@ function start (host = '127.0.0.1', port = 1433) {
     const raw = msg.toString();
     try {
       const data = JSON.parse(raw.split(' ').slice(7).join());
-      data.key = createId(data);
+      preprocess(data);
       await db.storeReading(data);
     } catch (error) {
       console.error('Handling Event failed with', error);
